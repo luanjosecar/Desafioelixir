108e8a67c6d7737dce3a2a886daebe86507c90c2
luanjosecar <luan.jose.c@gmail.com>
2022-01-31 22:11:46 -0300

    Adaptaçao ao Elixir

--- testes/lib/testes.ex
+++ testes/lib/testes.ex
@@ -1,32 +1,60 @@
 defmodule Testes do
-  @moduledoc """
-  Documentation for `Testes`.
-  """
 
-  @doc """
-  Hello world.
+  def verify_floats(list) do
+    if(Enum.all?(list, fn x -> not is_float(x) end)) do
+      IO.puts("Erro na leitura")
+    else
+      IO.puts("Todos os elementos são float")
+    end
+  end
 
-  ## Examples
 
-      iex> Testes.hello()
-      :world
 
-  """
+
+
+  def swap(list, pos_a, pos_b) do
+    temp = list.
+  end
+
+  def bubble_iner(,0,) do :nil
+  def bubble_iner(list, end_loop, start_loop \\ 0) do
+    if(Enum.at(list,end_loop) > Enum.at(list,end_loop+1))
+  end
+
+  def buuble_sort(,,0) do : nil
+  def bubble_sort(list, start_list, end_list) do
+    if(start_list == end_list) do
+      bubble_sort(list,start_list, 0)
+    else
+      bubble_iner(list, end_list-1-start_list)
+      bubble_sort(list, start+1, end_list)
+    end
+
+
+  end
+
   def hello do
     url = "http://challenge.dienekes.com.br/api/numbers?page=1"
 
-    case HTTPoison.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        IO.puts(Poison.decode!(body))
+    response = HTTPoison.get!(url)
+    req = Poison.decode!(response.body)
 
-      {:ok, %{status_code: 404}} ->
-        IO.puts("Erro1")
+    list = req["numbers"]
 
-      # do something with a 404
+    verify_floats(list)
 
-      {:error, %{reason: reason}} ->
-        IO.puts("Erro1")
-        # do something with an error
-    end
+    # case HTTPoison.get(url) do
+    #   {:ok, %{status_code: 200, body: body}} ->
+    #     modelo = Poison.decode!(body)
+
+    #   {:ok, %{status_code: 404}} ->
+    #     IO.puts("Erro1")
+
+    #   # do something with a 404
+
+    #   {:error, %{reason: reason}} ->
+    #     IO.puts("Erro1")
+    #     # do something with an error
+    # end
   end
 end
